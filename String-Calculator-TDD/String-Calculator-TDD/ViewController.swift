3762641383535415d6da5ac75ce4c3a32414b91d
Ilhansari <ilhansari94@hotmail.com>
2021-04-16 08:15:42 +0300

    Initiliaze new project string-calculator with tdd example

---
+++ String-Calculator-TDD/String-Calculator-TDD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  String-Calculator-TDD
+//
+//  Created by Ilhan Sari on 16.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ String-Calculator-TDD/String-Calculator-TDDTests/String_Calculator_TDDTests.swift
@@ -0,0 +1,13 @@
+//
+//  String_Calculator_TDDTests.swift
+//  String-Calculator-TDDTests
+//
+//  Created by Ilhan Sari on 16.04.2021.
+//
+
+import XCTest
+@testable import String_Calculator_TDD
+
+class String_Calculator_TDDTests: XCTestCase {
+
+}
