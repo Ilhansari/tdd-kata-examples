d0357a0e9b52a4ec6421c15d617671825328db6f
Ilhansari <ilhansari94@hotmail.com>
2021-04-18 15:34:10 +0300

    Handle amount of numbers test

--- String-Calculator-TDD/String-Calculator-TDDTests/String_Calculator_TDDTests.swift
+++ String-Calculator-TDD/String-Calculator-TDDTests/String_Calculator_TDDTests.swift
@@ -54,4 +54,10 @@ class String_Calculator_TDDTests: XCTestCase {
 
         XCTAssertEqual(sut.add("1;2;3;"), 0)
     }
+
+    func test_handleAmountOfNumbers() {
+        let sut = StringCalculator()
+
+        XCTAssertEqual(sut.add("1,2,3,4,5,6,7,8,9,10"), 55)
+    }
 }
