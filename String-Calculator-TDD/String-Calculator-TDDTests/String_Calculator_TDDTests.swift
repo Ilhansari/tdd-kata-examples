602d41a9ba78b5042d6ded9d28b33c75a9d7e406
Ilhansari <ilhansari94@hotmail.com>
2021-04-19 09:16:35 +0300

    Handle new lines between numbers

--- String-Calculator-TDD/String-Calculator-TDDTests/String_Calculator_TDDTests.swift
+++ String-Calculator-TDD/String-Calculator-TDDTests/String_Calculator_TDDTests.swift
@@ -10,18 +10,23 @@ import XCTest
 
 final class StringCalculator {
 
-    private let separators = CharacterSet.init(charactersIn: ",/n")
+    private let separators = CharacterSet.init(charactersIn: ",\n")
 
-    func add(_ values: String) -> Int {
-        return sumOfIntArray(convertStringToIntArray(values))
+    func add(_ values: String) throws -> Int {
+        do {
+            return try sumOfIntArray(convertStringToIntArray(values))
+        } catch let error {
+            throw error
+        }
     }
 
     private func convertStringToIntArray(_ stringValue: String) -> [Int?] {
         let valuesAsStringArray = stringValue.components(separatedBy: separators)
+
         return valuesAsStringArray.map { Int($0) }
     }
 
-    func sumOfIntArray(_ intArray: [Int?]) -> Int {
+    func sumOfIntArray(_ intArray: [Int?]) throws -> Int {
         guard intArray.filter ({ $0 == nil }).isEmpty else {
             return 0
         }
@@ -34,30 +39,36 @@ class String_Calculator_TDDTests: XCTestCase {
     func test_emptyString() {
         let sut = StringCalculator()
 
-        XCTAssertEqual(sut.add(""), 0)
+        XCTAssertEqual(try sut.add(""), 0)
     }
 
     func test_singleNumberReturnSameNumber() {
         let sut = StringCalculator()
 
-        XCTAssertEqual(sut.add("1"), 1)
+        XCTAssertEqual(try sut.add("1"), 1)
     }
 
     func test_twoNumberReturnSumOfNumber() {
         let sut = StringCalculator()
 
-        XCTAssertEqual(sut.add("1,2"), 3)
+        XCTAssertEqual(try sut.add("1,2"), 3)
     }
 
     func test_invalidInputReturnZero() {
         let sut = StringCalculator()
 
-        XCTAssertEqual(sut.add("1;2;3;"), 0)
+        XCTAssertEqual(try sut.add("1;2;3;"), 0)
     }
 
     func test_handleAmountOfNumbers() {
         let sut = StringCalculator()
 
-        XCTAssertEqual(sut.add("1,2,3,4,5,6,7,8,9,10"), 55)
+        XCTAssertEqual(try sut.add("1,2,3,4,5,6,7,8,9,10"), 55)
+    }
+
+    func test_handleNewLinesBetweenNumbers() {
+        let sut = StringCalculator()
+
+        XCTAssertEqual(try sut.add("1\n2\n3\n4\n5\n6\n7\n8\n9\n10"), 55)
     }
 }
